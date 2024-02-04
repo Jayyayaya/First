2eef837fe4f3fe23041290e7a471d7658ad71cf8
Jayyayaya <158851266+Jayyayaya@users.noreply.github.com>
2024-02-05 00:58:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+
+let popup = document.getElementById('popup');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let popup1 = document.getElementById('popup1');
+let popup2 = document.getElementById('popup2');
+let popup3 = document.getElementById('popup3');
+
+window.onload = function(){
+    openPopup();
+};
+function openPopup(){
+    popup.classList.add('open-popup');
+} 
+function closePopup(){
+    popup.classList.remove('open-popup');
+}
+function handleYes() {
+    alert('You are smart sweetie!\nGo ahead...');
+    closePopup();
+}
+    
+function handleNo() {
+    alert('There is no way to decline!\nClick on "Ofcourse"...');
+    openPopup();
+} 
+
+function openPopup1(){
+    popup1.classList.add('open-popup1');
+}
+function closePopup1(){
+    popup1.classList.remove('open-popup1');
+}
+
+function openPopup2(){
+    popup2.classList.add('open-popup1');
+}
+function closePopup2(){
+    popup2.classList.remove('open-popup1');
+}
+
+function openPopup3(){
+    popup3.classList.add('open-popup1');
+}
+function closePopup3(){
+    popup3.classList.remove('open-popup1');
+}
+
+
+// document.querySelector('#popup').addEventListener('wheel', preventScroll, {passive: false});
+// function preventScroll(e){
+//     e.preventDefault();
+//     e.stopPropagation();
+
+//     return false;
+// }
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
